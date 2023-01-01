0bf98eb94b41650a7f4fa2c92d3416d5009d2baa
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-01-01 10:33:16 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './Home';
+
+import { UserProvider } from './context/user';
+
+export default function App() {
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  )
+}
