ff8ccd97423eaca90c42dad9b1f55576b1c3e623
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-01-02 06:35:38 -0500

    grid initialized

--- src/Home.js
+++ src/Home.js
@@ -8,21 +8,25 @@ import corner from './img/Corner.png'
 export default function () {
 
   return (
-    <div>
+    <div className='elixirs-and-alienists'>
         <div className='grid'>
-            <div className='checked'>
-                <img src={checked} alt='check' />
-                <p>Welcome to Paradox</p>
-            </div>
-            <div className='line'>
-                <img src={line} alt="line" />
-            </div>
+            {/* <div className='checked'>
+                <img src={checked} alt='check' /> */}
+                {/* <p>Welcome to Paradox</p> */}
+            {/* </div>
+             */}
+             <div></div>
+             <div></div>
             <div className='unchecked'>
                 <img src={unchecked} alt='unchecked' />
             </div>
-            <div className='corner-line'>
-                <img src={corner} alt='corner' />
+            <div></div><div></div><div></div><div></div>
+            <div className='line'>
+                <img src={line} alt="line" />
             </div>
+            {/* <div className='corner-line'>
+                <img src={corner} alt='corner' />
+            </div> */}
         </div>
     </div>
   )
