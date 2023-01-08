6e46d80264af48f95eab2b3246d916ceb4cfb4d7
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-01-07 23:53:50 -0500

    unchecked box component added

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import checked from './img/Checked.png'
+import UncheckedBox from './UncheckedBox'
+
+// import checked from './img/Checked.png'
 import line from './img/Line.png'
-import unchecked from './img/unchecked.png'
+// import unchecked from './img/unchecked.png'
 import corner from './img/Corner.png'
 
 export default function () {
@@ -12,21 +14,135 @@ export default function () {
         <div className='grid'>
             {/* <div className='checked'>
                 <img src={checked} alt='check' /> */}
-                {/* <p>Welcome to Paradox</p> */}
             {/* </div>
              */}
-             <div></div>
-             <div></div>
-            <div className='unchecked'>
-                <img src={unchecked} alt='unchecked' />
-            </div>
-            <div></div><div></div><div></div><div></div>
+             <div/><div/><div/><div/>
+            <UncheckedBox />
+            <div/><div/><div/><div/><div/><div/><div/><div/>
             <div className='line'>
                 <img src={line} alt="line" />
             </div>
-            {/* <div className='corner-line'>
+            <div/><div/><div/><div/><div/><div/><div/><div/>
+            <UncheckedBox />
+            <div/><div/><div/><div/><div/><div/><div/>
+            <UncheckedBox />
+            <div className='crossed-lines'>
+                <img src={line} alt="line" />
+                <img src={line} alt="line" className='rotated-line'/>
+            </div>
+            <UncheckedBox />
+            <div/><div/><div/><div/><div/><div/>
+            <div className='corner-se'>
                 <img src={corner} alt='corner' />
-            </div> */}
+            </div>
+            <div className='line'>
+                <img src={line} alt="line" className='horizontal-line'/>
+            </div>
+            <div className='corner-sw'>
+                <img src={corner} alt='corner' />
+            </div>
+            <div/><div/><div/><div/><div/><div/>
+            <UncheckedBox />
+            <div className='corners-down'>
+                <img src={corner} alt='corner' className='corner-down-left' />
+                <img src={corner} alt='corner' className='corner-down-right' />
+            </div>
+            <UncheckedBox />
+            <div/><div/><div/><div/><div/><div/>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <UncheckedBox />
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div/><div/><div/><div/><div/><div/>
+            <UncheckedBox />         
+            <div className='corners-up'>
+                <img src={corner} alt='corner' className='corner-up-left' />
+                <img src={corner} alt='corner' className='corner-up-right' />
+            </div>
+            <UncheckedBox />
+            <div/><div/><div/><div/><div/><div/>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <UncheckedBox /> 
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div/><div/><div/><div/>
+            <UncheckedBox /> 
+            <div className='line'>
+                <img src={line} alt="line" className='horizontal-line'/>
+            </div>
+            <UncheckedBox /> 
+            <div className='crossed-lines'>
+                <img src={line} alt="line" />
+                <img src={line} alt="line" className='rotated-line'/>
+            </div>
+            <UncheckedBox /> 
+            <div className='line'>
+                <img src={line} alt="line" className='horizontal-line'/>
+            </div>
+            <UncheckedBox /> 
+            <div/><div/><div/><div/>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <UncheckedBox />
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div/><div/><div/><div/><div/><div/>
+            <UncheckedBox />
+            <div className='corners-down'>
+                <img src={corner} alt='corner' className='corner-down-left' />
+                <img src={corner} alt='corner' className='corner-down-right' />
+            </div>
+            <UncheckedBox /> 
+            <div/><div/><div/><div/><div/><div/>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <UncheckedBox /> 
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div/><div/><div/><div/><div/><div/>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div className='corners-down'>
+                <img src={corner} alt='corner' className='corner-down-left' />
+                <img src={corner} alt='corner' className='corner-down-right' />
+            </div>
+            <div className='line'>
+                <img src={line} alt="line"/>
+            </div>
+            <div/><div/><div/><div/><div/>
+            <div className='corners-down'>
+                <img src={corner} alt='corner' className='corner-down-right' />
+            </div>
+            <div className='corners-up'>
+                <img src={corner} alt='corner' className='corner-up-left' />
+            </div>
+            <UncheckedBox />
+            <div className='corners-up'>
+                <img src={corner} alt='corner' className='corner-up-right' />
+            </div>
+            <div className='corners-down'>
+                <img src={corner} alt='corner' className='corner-down-left' />
+            </div>
+            <div/><div/><div/><div/>
+            <UncheckedBox />
+            <UncheckedBox />
+            <div className='corners-up'>
+                <img src={corner} alt='corner' className='corner-up-left' />
+                <img src={corner} alt='corner' className='corner-up-right' />
+            </div>
+            <UncheckedBox />
+            <UncheckedBox />
         </div>
     </div>
   )
---
+++ src/UncheckedBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import unchecked from './img/unchecked.png'
+
+export default function UncheckedBox() {
+  return (
+    <div className='unchecked'>
+        <img src={unchecked} alt='unchecked' />
+        <p className='no-symptoms'>No Symptoms</p>
+    </div>
+  )
+}
