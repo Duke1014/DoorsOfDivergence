deeaedf6adb74ea668ce0acd8dfd0c8953d6624d
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-03-13 00:12:02 -0400

    content lib updated

--- src/lib/content.js
+++ src/lib/content.js
@@ -5,24 +5,326 @@ export const uiContent = {
         two: { title: 'An Invitation' },
         three: { title: 'Stubborn' },
         four: { title: 'Sins of the Father' },
-        six: { title: 'Beyond the Veil' },
-        five: { title: 'To the Nines' },
-        nine: { title: 'Out of Time' },
+        five: { title: 'Beyond the Veil' },
+        six: { title: 'To the Nines' },
+        seven: { title: 'Out of Time' },
         eight: { title: 'Pandora\'s Box' },
-        ten: { title: 'A Soul in Limbo' },
-        eleven: { title: 'You Were Warned' },
-        seven: { title: 'What Was Lost' },
-        fourteen: { title: 'Machine of Blood' },
-        fifteen: { title: 'Chains that Bind' },
-        sixteen: { title: 'Mors Ex Machina' },
-        seventeen: { title: 'Unbound' },
-        twelve: { title: 'Training Wheels' },
-        thirteen: { title: 'Your Own Two Feet' },
-        eighteen: { title: 'Good Housekeeping' },
+        nine: { title: 'Beyond the Veil' },
+        ten: { title: 'Alchemist' },
+        eleven: { title: 'What Was Lost' },
+        twelve: { title: 'Machine of Blood' },
+        thirteen: { title: 'Chains That Bind' },
+        fourteen: { title: 'Mors Ex Machina' },
+        fifteen: { title: 'Unbound' },
+        sixteen: { title: 'Theurgist' },
+        seventeen: { title: 'Oracle' },
+        eighteen: { title: 'Honesty' },
         nineteen: { title: 'Out With a Bang' },
         twenty: { title: 'Death Deferred' },
-        twentyone: { title: 'Broken Chains' },
+        twentyone: { title: 'Misled' },
         twentytwo: { title: 'Better Safe Than Sorry' },
+    },
+    madnessNodes: {
+        one: { 
+            title: 'Alienist In Training',
+            description: "You took a position at Hollow Grove Sanitarium"
+        },
+        two: {
+            title: "Oblivious",
+            description: "You entered Hollow Grove Sanitarium"
+        },
+        three: {
+            title: "Narrow-minded",
+            description: "You chose the same color serums"
+        },
+        four: {
+            title: "Unmotivated",
+            description: "You kept the doctor waiting"
+        },
+        five: {
+            title: "Sound of Mind",
+            description : "You chose more blue Serums"
+        },
+        six: {
+            title: "Sound of Body",
+            description : "You chose more red Serums"
+        },
+        seven: {
+            title: "Critical Thinker",
+            description : "Discovered Charles's true motivations"
+        },
+        eight: {
+            title: "Strong Willed",
+            description : "Discovered Grace's true motivations"
+        },
+        nine: {
+            title: "Anxious",
+            description : "Learned what was done to Charles"
+        },
+        ten: {
+            title: "Pyromania",
+            description : "You learned what was done to Grace"
+        },
+        eleven: {
+            title: "Thanatopobia",
+            description : "Witnessed Edmond's end at Charles' hands"
+        },
+        twelve: {
+            title: "Hot-headed",
+            description : "Witnessed Edmond's End at Grace's hands"
+        },
+        thirteen: {
+            title: "Electrophilia",
+            description : "Shocked a friend to secure your escape"
+        },
+        fourteen: {
+            title: "Epiplaphobia",
+            description : "Took a seat in the Electric Chair"
+        },
+        fifteen: {
+            title: "Gambler",
+            description : "You saved your ally by playing the Odds"
+        },
+        sixteen: {
+            title: "Left to Starve",
+            description : "You were abandoned"
+        },
+        seventeen: {
+            title: "Cruel Calculus",
+            description : "You killed Grace"
+        },
+        eighteen: {
+            title: "All the Things I Might Have Said",
+            description : "You saved Grace"
+        },
+        nineteen: {
+            title: "A Cleansing Flame",
+            description : "You saved Charles"
+        },
+        twenty: {
+            title: "No Second Chances",
+            description : "You killed Charles"
+        },
+        twentyone: {
+            title: "Indecisive",
+            description : "You were given one final chance to save your life."
+        },
+        twentytwo: {
+            title: "Out With A Bang",
+            description : "You died at Charles' hands"
+        },
+        twentythree: {
+            title: "Blaze of Glory",
+            description : "You were burned alive by Grace"
+        },
+        twentyfour: {
+            title: "Narcoleptic",
+            description : "Went to sleep"
+        },
+        twentyfive: {
+            title: "Narcissistic",
+            description : "Put on clothes that don't belong to you"
+        },
+        twentysix: {
+            title: "Co-ordinated",
+            description : "Impressed Hamlin with your Dancing Skills"
+        },
+        twentyseven: {
+            title: "Vocal",
+            description : "Impressed Hamlin with your singing skills"
+        },
+        twentyeight: {
+            title: "Obstinate",
+            description : "Never rang the bell"
+        },
+        twentynine: {
+            title: "Arrogant",
+            description : "This still isn't a Marvel Movie."
+        },
+        thirty: {
+            title: "Mission of Mercy",
+            description : "Received word of Edmond's whereabouts"
+        },
+        thirtyone: {
+            title: "Nepotism",
+            description : "Encountered Edmond's Children"
+        },
+        thirtyfour: {
+            title: "Inactive",
+            description : "You left the Doctor waiting"
+        },
+        thirtyfive: {
+            title: "Sanguine",
+            description : "You discovered more about Edmond's activities"
+        },
+        thirtysix: {
+            title: "Frequent Attacks",
+            description : "You learned more about the family history"
+        },
+        thirtyseven: {
+            title: "Kleptomania",
+            description : "You stole a key"
+        },
+        thirtyeight: {
+            title: "Lethargic",
+            description : "You left the key behind"
+        },
+        thirtynine: {
+            title: "Insubordinate",
+            description : "You refused to do as you were told."
+        },
+        fourty: {
+            title: "Patricidal",
+            description : "You witnessed the end of Edmond Cavanaugh"
+        },
+        fourtyone: {
+            title: "Disloyal",
+            description : "You brokered a deal with Constance"
+        },
+        fourtytwo: {
+            title: "Draining Day",
+            description : "You failed to escape Hollow Grove in time."
+        },
+        fourtythree: {
+            title: "Bloodthirsty",
+            description : "You chose to kill Constance"
+        },
+        fourtyfour: {
+            title: "Terminal Illness",
+            description : "You failed to save Constance"
+        },
+        fourtyfive: {
+            title: "Forever And A Day",
+            description : "You saved Constance"
+        },
+        fourtysix: {
+            title: "Reckless",
+            description : "You lived three lives in one night."
+        },
+        fourtyseven: {
+            title: "Unhinged Behavior",
+            description : "You were sent to Hollow Grove Sanitarium"
+        },
+        fourtyeight: {
+            title: "Knowledgeabe",
+            description : "Reminded Tidwell of the Swiss Alienist's name"
+        },
+        fourtynine: {
+            title: "Committed",
+            description : "Met Tidwell and Doss"
+        },
+        fifty: {
+            title: "Retrophilic",
+            description : "Rejected the modern for the classic"
+        },
+        fiftyone: {
+            title: "Neophilic",
+            description : "Rejected the classic for the modern"
+        },
+        fiftytwo: {
+            title: "Intractible",
+            description : "You left the Doctor waiting"
+        },
+        fiftythree: {
+            title: "Paranoid",
+            description : "Learned more about Tidwell's practices"
+        },
+        fiftyfour: {
+            title: "Like I Need A Hole In The Head",
+            description : "Lobotomized by Doctor Tidwell"
+        },
+        fiftyfive: {
+            title: "Dishonest",
+            description : 'Revealed "Tidwell"s true nature'
+        },
+        fiftysix: {
+            title: "The One Made Many",
+            description : '"Tidwell" ripped Lamashtu from you'
+        },
+        fiftyseven: {
+            title: "Deceitful",
+            description : "Revealed Doss's true nature"
+        },
+        fiftyeight: {
+            title: "The Many Made One",
+            description : "Lamashtu took her soul back from you"
+        },
+        fiftynine: {
+            title: "Uneasy",
+            description : 'Brokered an alliance with "Tidwell"'
+        },
+        sixty: {
+            title: "Opportunistic",
+            description : 'Did not release your fellow inmates before making your choice'
+        },
+        sixtyone: {
+            title: "Better The Devil You Know",
+            description : "Sided with Lamashtu a second time."
+        },
+        sixtytwo: {
+            title: "Gone For Good (?)",
+            description : "Banished Lamashtu"
+        },
+        sixtythree: {
+            title: "A Lead",
+            description : "Discovered Edmond's whereabouts"
+        },
+        sixtyfour: {
+            title: "Combative",
+            description : "Confronted Doctor Tidwell"
+        },
+        sixtyfive: {
+            title: "Hesitant",
+            description : "Escorted from Hollow Grove Prematurely"
+        },
+        sixtysix: {
+            title: "Hallucinations",
+            description : "Received visions of Eveline"
+        },
+        sixtyseven: {
+            title: "Taciturn",
+            description : "Refused to answer Tidwell's questions"
+        },
+        sixtyeight: {
+            title: "Confused",
+            description : 'Learned more about what Edmond was doing.'
+        },
+        sixtynine: {
+            title: "Impatient",
+            description: "Removed the Blindfold"
+        },
+        seventy: {
+            title: "Unflappable",
+            description : "Didn't remove the blindfold"
+        },
+        seventyone: {
+            title: "Dumbfounded",
+            description : "You began to question reality"
+        },
+        seventytwo: {
+            title: "Invasive Procedures",
+            description : "Tidwell was forced to try a more extreme treatment"
+        },
+        seventythree: {
+            title: "Fickle",
+            description : "Faced a decision on who to believe"
+        },
+        seventyfour: {
+            title: "A New Day",
+            description : "You chose to embrace the truth"
+        },
+        seventyfive: {
+            title: "Finally Free",
+            description : 'You released Eveline from her imprisonment'
+        },
+        seventysix: {
+            title: "Headstrong",
+            description : "You kept the doctor waiting"
+        },
+        seventyseven: {
+            title: "Delusions of Grandeur",
+            description : "You saw a message from the future"
+        }
     },
     general: {
         not_unlocked: 'You haven\'t unlocked \nthis part of the story yet.',
